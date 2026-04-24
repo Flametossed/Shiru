721decc3f7aa59b478ec50136c40cc5ebff5228c
RockinChaos <10971577+RockinChaos@users.noreply.github.com>
2026-04-24 15:20:28 -0700

    fix: prevent status bar transition on orientation change
    Disabled the status bar transition during orientation changes on Android to prevent the bar from slowly resizing when rotating the device.

--- common/components/Status.svelte
+++ common/components/Status.svelte
@@ -2,15 +2,30 @@
   import { Earth, WifiOff, CloudAlert } from 'lucide-svelte'
   import { status } from '@/modules/networking.js'
   import { SUPPORTS } from '@/modules/support.js'
+  import { onDestroy } from 'svelte'
+
+  let transition = true
   $: {
     const root = document.documentElement
     if ($status.match(/offline/i)) root.style.setProperty('--wrapper-offset', getComputedStyle(root).getPropertyValue('--statusbar-height').trim())
     else root.style.removeProperty('--wrapper-offset')
   }
+
+  function onOrientation() {
+    if ($status.match(/offline/i)) {
+      transition = false
+      requestAnimationFrame(() => transition = true)
+    }
+  }
+
+  if (SUPPORTS.isAndroid) {
+    screen.orientation.addEventListener('change', onOrientation)
+    onDestroy(() => screen.orientation.removeEventListener('change', onOrientation))
+  }
 </script>
 
-<div class='overflow-hidden status-bar h-0' class:offline={!SUPPORTS.isAndroid && $status.match(/offline/i)} class:offline-safe={SUPPORTS.isAndroid && $status.match(/offline/i)}>
-  <div class='z-101 position-absolute w-full d-flex align-items-center justify-content-center overflow-hidden status-bar h-0' class:offline={!SUPPORTS.isAndroid && $status.match(/offline/i)} class:offline-safe={SUPPORTS.isAndroid && $status.match(/offline/i)} class:padding-safe={SUPPORTS.isAndroid} class:bg-very-dark={$status.match(/offline/i)} class:bg-success={!$status.match(/offline/i)}>
+<div class='overflow-hidden status-bar h-0' class:status-bar-transition={transition} class:offline={!SUPPORTS.isAndroid && $status.match(/offline/i)} class:offline-safe={SUPPORTS.isAndroid && $status.match(/offline/i)}>
+  <div class='z-79 position-absolute w-full d-flex align-items-center justify-content-center overflow-hidden status-bar h-0' class:status-bar-transition={transition} class:offline={!SUPPORTS.isAndroid && $status.match(/offline/i)} class:offline-safe={SUPPORTS.isAndroid && $status.match(/offline/i)} class:padding-safe={SUPPORTS.isAndroid} class:bg-very-dark={$status.match(/offline/i)} class:bg-success={!$status.match(/offline/i)}>
     {#if $status === 'online'}
       <Earth size='1.8rem' strokeWidth='2.5' />
       <span class='ml-10 font-weight-semi-bold font-size-16'>Connection Restored</span>
@@ -22,10 +37,6 @@
 </div>
 
 <style>
-  .status-bar {
-    transition: height 0.3s ease, padding-top 0.3s ease;
-    transition-delay: 2s;
-  }
   .status-bar.offline {
     height: var(--statusbar-height);
   }
@@ -34,5 +45,9 @@
   }
   .status-bar.offline-safe.padding-safe {
     padding-top: env(safe-area-inset-top, 0);
+  }
+  .status-bar-transition {
+    transition: height 0.3s ease, padding-top 0.3s ease;
+    transition-delay: 2s;
   }
 </style>
