0b5d07056fdeee0464ff48db7ca8a78940e2255c
RockinChaos <10971577+RockinChaos@users.noreply.github.com>
2026-04-30 19:17:38 -0700

    chore: improve episode skeleton card

--- common/components/skeletons/EpisodeCardSk.svelte
+++ common/components/skeletons/EpisodeCardSk.svelte
@@ -5,12 +5,13 @@
 <div class='d-flex p-20 pb-10 position-relative wrapper content-visibility-auto' class:mb-150={section}>
   <div class='item load-in d-flex flex-column h-full'>
     <div class='image h-200 w-full rounded skeloader bg-ske'/>
-    <div class='row pt-15'>
-      <div class='col-7'>
-        <div class='h-10 skeloader w-150 bg-ske rounded '/>
+    <div class='row pt-20'>
+      <div class='col-7 d-flex flex-column'>
+        <div class='h-15 skeloader w-220 bg-ske mb-8 rounded'/>
+        <div class='h-10 skeloader w-180 bg-ske rounded'/>
       </div>
       <div class='col-5 d-flex flex-column align-items-end'>
-        <div class='h-10 skeloader w-100 bg-ske mb-5 rounded'/>
+        <div class='h-15 skeloader w-100 bg-ske mb-8 rounded'/>
         <div class='h-10 skeloader w-50 bg-ske rounded'/>
       </div>
     </div>
@@ -19,7 +20,9 @@
 
 <style>
   .item {
-    width: 36rem
+    width: 36rem;
+    max-width: 100%;
+    margin: 0 auto;
   }
   .wrapper {
     height: 28.7rem;
